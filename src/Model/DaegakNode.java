89b17f50bdaa2defd88986b2e5525bebfbbb3b0e
eunjilee0311 <leg040311@gmail.com>
2025-05-10 03:05:09 +0900

    UI 저장

---
+++ src/Model/DaegakNode.java
@@ -0,0 +1,9 @@
+package Model;
+
+
+public class DaegakNode extends Node {
+    DaegakNode(int id, int x, int y) {
+        super(id, x, y);
+        this.isDaegak = true;
+    }
+}
---
+++ src/Model/Horse.java
@@ -0,0 +1,18 @@
+package Model;
+
+public class Horse {
+    public int id;
+    public int x, y, width, height;
+    public Node currentNode;
+    public Node prevNode;
+
+    // 생성자
+    public void Horse(){};
+
+    public void move(){
+        x = x++;     //
+        y = y++;    // 이런 식으로 x, y,도 어떻게 움직일지 알아야
+
+    }
+
+}
