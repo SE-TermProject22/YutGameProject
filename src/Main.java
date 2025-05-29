154ed2f76093cdb7b4d8918ab9a79a1ebb100c7d
minddoi <minddoi493@gmail.com>
2025-05-29 16:55:41 +0900

    빠진 이미지 추가

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String uiMode = "swing";
+        String uiMode = "fx";
 
         if (uiMode.equalsIgnoreCase("swing")) {
             SwingUI();
