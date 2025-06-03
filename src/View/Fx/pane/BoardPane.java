f9bceb81b2e8a4afb0fc3dbdea706c1924ea4670
minddoi <minddoi493@gmail.com>
2025-06-03 14:10:02 +0900

    (fx)gameView pane 분리 완료

---
+++ src/View/Fx/pane/BoardPane.java
@@ -0,0 +1,25 @@
+package View.Fx.pane;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class BoardPane extends AnchorPane {
+
+    private final ImageView boardView;
+
+    public BoardPane() {
+        boardView = new ImageView();
+        this.getChildren().add(boardView);
+    }
+
+    public void setBoardImage(String boardType) {
+        String path = "/image/" + boardType + " board.png";
+        Image image = new Image(getClass().getResourceAsStream(path));
+        boardView.setImage(image);
+    }
+
+    public ImageView getBoardView() {
+        return boardView;
+    }
+}
