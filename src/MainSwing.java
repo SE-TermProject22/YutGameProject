aa3805cff6366ce84789cb8f9ffb39c2c1e15b85
yenalee <lyemee@naver.com>
2025-06-04 19:25:54 +0900

    jar file

--- src/MainSwing.java
+++ src/MainSwing.java
@@ -2,41 +2,24 @@ import javax.swing.*;
 import Controller.SwingGameController;
 import View.Swing.StartView;
 import View.Swing.GameView;
-
-import View.Fx.MainFX;
 import View.Swing.EndView;
 
-public class Main {
-
+public class MainSwing {
     public static void main(String[] args) {
-
-        String uiMode = "swing";
-
-        if (uiMode.equalsIgnoreCase("swing")) {
-            SwingUI();
-        } else if (uiMode.equalsIgnoreCase("fx")) {
-            MainFX.launchApp();
-        }
-    }
-
-    private static void SwingUI() {
-        // Swing UI는 이벤트 디스패치 스레드에서 실행해야 안전
         SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Horse Game(javaSwing)");
+            JFrame frame = new JFrame("Horse Game (Swing)");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setSize(1100, 700);
-            frame.setLocationRelativeTo(null); // 화면 중앙에 배치
+            frame.setLocationRelativeTo(null);
             frame.setResizable(false);
 
-            // 뷰들 생성
             StartView startView = new StartView();
             GameView gameView = new GameView();
             EndView endView = new EndView();
 
-            gameView.setVisible(false); // 처음엔 안 보이게
+            gameView.setVisible(false);
             endView.setVisible(false);
 
-            // 패널들을 JFrame에 추가
             frame.setLayout(null);
             startView.setBounds(0, 0, 1100, 700);
             gameView.setBounds(0, 0, 1100, 700);
@@ -46,7 +29,6 @@ public class Main {
             frame.add(gameView);
             frame.add(endView);
 
-            // 컨트롤러 생성
             new SwingGameController(frame, startView, gameView, endView);
 
             frame.setVisible(true);
